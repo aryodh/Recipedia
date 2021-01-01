6ad7f7043f534ab3a8459ce39432909f3e6d8504
aryo <ardh.yodh@gmail.com>
2021-01-01 15:43:00 +0700

    Add OpenGL

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/aryodh/recipedia/SplashScreenActivity.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/aryodh/recipedia/SplashScreenActivity.kt
@@ -3,6 +3,7 @@ package id.ac.ui.cs.mobileprogramming.aryodh.recipedia
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.opengl.GLSurfaceView
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -11,6 +12,8 @@ import id.ac.ui.cs.mobileprogramming.aryodh.recipedia.ui.main.MainFragment
 class SplashScreenActivity : AppCompatActivity() {
     private lateinit var sharedPref: SharedPreferences
     private val SPLASH_TIME_OUT:Long = 2000 // 2 sec
+    private lateinit var gLView: GLSurfaceView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -19,7 +22,9 @@ class SplashScreenActivity : AppCompatActivity() {
         val editor: SharedPreferences.Editor = sharedPref.edit()
         editor.putBoolean("fromBR", false)
         editor.apply()
-        setContentView(R.layout.splashscreen)
+        gLView = MyGLSurfaceView(this)
+        setContentView(gLView)
+
         Handler().postDelayed({
             startActivity(Intent(this,MainActivity::class.java))
             finish()
